0323042f32e4f2961d5c01efb6b3472cb45f98f1
David <perzc.david@gmail.com>
2018-02-24 20:00:32 -0500

    add eureka client config

--- animePersist/src/main/java/com/anime/persist/controller/AnimeController.java
+++ animePersist/src/main/java/com/anime/persist/controller/AnimeController.java
@@ -1,6 +1,7 @@
 package com.anime.persist.controller;
 
 import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
